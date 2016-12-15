22eaae07988b4bf4cee5b80ab247fc6630d81ec5
Eric Lau <elau002@gmail.com>
2016-12-14 16:04:41 -0800

    Table now highlights user position relative to other

--- client/leaderBoard/leaderBoard.js
+++ client/leaderBoard/leaderBoard.js
@@ -1,8 +1,31 @@
-angular.module('rehjeks.leaderboard', [])
+angular.module('rehjeks.leaderboard', [
+  'ngCookies'
+])
 
-  .controller('leaderBoardController', function ($scope, Server) {
+  .controller('leaderBoardController', function ($scope, Server, $cookies, $sce) {
+    $scope.username = $cookies.get('username');
     $scope.leaders = [];
+    $scope.listLimit = 10;
     $scope.getUsers = function () {
       Server.getUsers($scope);
     }
+
+    $scope.highlight = function () {
+      var currentUser = $scope.username;
+      var highlightedText = $scope.username.replace(currentUser, '<span class="highlighted-text">$&</span');
+      $scope.highlightedText = $sce.trustAsHtml(highlightedText);
+    }
+
+    $scope.findUser = function (name) {
+      if (name === $scope.username) {
+        $scope.highlight();
+        return true;
+      }
+    }
+
+    $scope.$watch(function () { return $cookies.get('username'); }, function () {
+      var username = $cookies.get('username');
+      $scope.username = username;
+    });
+
   })
