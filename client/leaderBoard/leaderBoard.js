c40832bf68a00ec99af3e60c6eed5602aa6f9507
Eric Lau <elau002@gmail.com>
2016-12-15 10:38:33 -0800

    highlighting now colors user's row

--- client/leaderBoard/leaderBoard.js
+++ client/leaderBoard/leaderBoard.js
@@ -8,39 +8,44 @@ angular.module('rehjeks.leaderboard', [
     $scope.filteredleaders = [];
 
     $scope.getUsers = function () {
+      //grab all users from the database
       Server.getUsers($scope)
         .then(function () {
           var counter = 0;
+         //after the promise is returned, sort by user's score
           $scope.leaders.sort(function (a, b) {
             return a.score - b.score;
           }).forEach(function (item) {
+          //create an index key equal to the counter to keep track of position in array
             item.index = counter;
+            // then we increment our counter
             counter++;
           })
+          //iterate over the now sorted array of objects
           for (var i = 0; i < $scope.leaders.length; i++) {
-            console.log($scope.leaders[i].username)
+            //check if current index is equal to current user
+              //if true and user is within the top 10
+                //return the first 10 entries
             if ($scope.leaders[i].username === $scope.username && i < 10) {
-              console.log('beginning', i)
               return $scope.filteredleaders = $scope.leaders.slice(0, 10);
-            } else if ($scope.leaders[i].username === $scope.username && $scope.leaders[i + 9] !== undefined && i >= 10) {
-              console.log('middle', i)
+            }
+              // if true and user is not in the top 10
+                //check if user has 9 users below them
+                  // if true return the users position with the 9 after them 
+            else if ($scope.leaders[i].username === $scope.username && !!$scope.leaders[i + 9] && i >= 10) {
               return $scope.filteredleaders = $scope.leaders.slice(i - 1, i + 9);
-            } else if ($scope.leaders[i].username === $scope.username && $scope.leaders[i + 9] === undefined) {
+            }
+              // lastly if the first two condition are false, but the user exist
+                // return the last ten entries
+            else if ($scope.leaders[i].username === $scope.username && !$scope.leaders[i + 9]) {
               return $scope.filteredleaders = $scope.leaders.slice($scope.leaders.length - 10, $scope.leaders.length);
             }
           }
         })
     }
 
-    $scope.highlight = function () {
-      var currentUser = $scope.username;
-      var highlightedText = $scope.username.replace(currentUser, '<span class="highlighted-text">$&</span');
-      $scope.highlightedText = $sce.trustAsHtml(highlightedText);
-    }
-
-    $scope.findUser = function (name) {
-      if (name === $scope.username) {
-        $scope.highlight();
+    $scope.findUser = function (user) {
+      if (user.username === $scope.username) {
         return true;
       }
     }
@@ -49,10 +54,4 @@ angular.module('rehjeks.leaderboard', [
       var username = $cookies.get('username');
       $scope.username = username;
     });
-
-    // $scope.filterleaders = function (user) {
-    //   $scope.getUsers();
-    //   console.log($scope.leaders);
-    //   console.log(user)
-    // }
   })
